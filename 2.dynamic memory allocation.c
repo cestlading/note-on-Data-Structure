633d90af63995ae02b659e7316d7253f8039dc12
cestlading <cestlading@gmail.com>
2019-03-03 20:56:06 +0800

    Add files via upload

--- 2.dynamic memory allocation.c
+++ 2.dynamic memory allocation.c
@@ -11,13 +11,13 @@ int main(){
 
 	int i,*pi;
 	float f, *pf;
-	pi = (int*) malloc(sizeof(int));
-	pf = (float*) malloc(sizeof(float));
+	pi = (int*) malloc(sizeof(int));//dynamically allocation with size int
+	pf = (float*) malloc(sizeof(float));//dynamically allocation with size float
 	*pi = 1024;
 	*pf = 3.14;
 	printf("i = %d, f = %lf\n",*pi,*pf);
-	free(pi);
-	free(pf);
+	free(pi);//free the memory of pointer pi
+	free(pf);;//free the memory of pointer pf
 
 	system("pause");
 }
